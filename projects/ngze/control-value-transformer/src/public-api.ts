00fbecf36085beed260bfd76cbb68be32359d817
ZeevKatz <zeevk6@gmail.com>
2020-10-15 22:02:00 +0300

    feat: 🎸 implement control value transformer metadata decorator

--- projects/ngze/control-value-transformer/src/public-api.ts
+++ projects/ngze/control-value-transformer/src/public-api.ts
@@ -1,3 +1,12 @@
 /*
  * Public API Surface of Control Value Transformer
  */
+
+import 'reflect-metadata';
+
+export {
+  ControlValueTransformer,
+  ControlValueTransformerMetadata,
+  getControlValueTransformerMetadata,
+} from './lib/metadata';
+export { Transformer } from './lib/shared';
