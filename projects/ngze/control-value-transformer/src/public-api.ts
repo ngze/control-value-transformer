7989a78f7fd227aff43ad2b29aaca137e09dc26a
ZeevKatz <zeevk6@gmail.com>
2020-10-15 22:16:17 +0300

    feat: 🎸 implement control value transformer directive

--- projects/ngze/control-value-transformer/src/public-api.ts
+++ projects/ngze/control-value-transformer/src/public-api.ts
@@ -7,3 +7,5 @@ import 'reflect-metadata';
 export { ControlValueTransformer, ControlValueTransformerMetadata, getTransformerMetadata } from './lib/metadata';
 export { Transformer } from './lib/shared';
 export { ControlValueTransformerRegister } from './lib/register';
+export { ControlValueTransformerDirective } from './lib/directive';
+export { ControlValueTransformerModule } from './lib/control-value-transformer.module';
