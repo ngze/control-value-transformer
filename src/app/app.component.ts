727a2c45fc1efbb25f0b46515264aef0339191a0
ZeevKatz <zeevk6@gmail.com>
2020-10-23 10:37:29 +0300

    feat: 🎸 add more examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PrefixTransformer } from './prefix.transformer';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   html: string;
   password: string;
+  number: number;
+  url: string;
+  httpsPrefixTransformer = new PrefixTransformer('https://');
 }
